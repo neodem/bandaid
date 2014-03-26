3944857931676909f16cfd6afd7363cff7f9f2ea
Vincent Fumo <vincent_fumo@cable.comcast.com>
2014-03-26 14:50:00 -0400

    misc

--- src/main/java/com/neodem/bandaid/game/GameContext.java
+++ src/main/java/com/neodem/bandaid/game/GameContext.java
@@ -14,7 +14,7 @@ public abstract class GameContext<P extends Player> {
     protected abstract Log getLog();
 
     // a list of the players in the game (ordered by their position in the game)
-    protected List<P> players = new ArrayList<P>();
+    protected List<P> players = new ArrayList<>();
 
     public void addPlayer(P p) {
         players.add(p);
@@ -27,7 +27,7 @@ public abstract class GameContext<P extends Player> {
     @Override
     public String toString() {
 
-        StringBuffer b = new StringBuffer();
+        StringBuilder b = new StringBuilder();
         b.append("Game Context\n");
         b.append("------------\n");
         b.append(players.size());
